ad9c88b02801e37d58bfe07af1154becc6261756
abitwitch <arrakis256@gmail.com>
2023-07-20 22:28:58 -0400

    trying to get to work

--- Screen/TelegraphDisplay.py
+++ Screen/TelegraphDisplay.py
@@ -16,7 +16,7 @@ class Screen(object):
         self.DC = 24
         self.SPI_PORT = 0
         self.SPI_DEVICE = 0
-        self.disp = SSD1305.SSD1305_128_32(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
+        self.disp = SSD1305.SSD1305_128_32(rst=self.RST, dc=self.DC, spi=SPI.SpiDev(self.SPI_PORT, self.SPI_DEVICE, max_speed_hz=8000000))
         self.disp.begin()
         # Clear display.
         self.disp.clear()
@@ -33,10 +33,10 @@ class Screen(object):
     def writeLines(self,line1="",line2="",line3="",line4=""):
         self.draw.rectangle((0,0,self.width,self.height), outline=0, fill=0)
         x=0
-        self.draw.text((x, self.top),    line1, font=font, fill=255)
-        self.draw.text((x, self.top+8),  line2, font=font, fill=255)
-        self.draw.text((x, self.top+16), line3, font=font, fill=255)
-        self.draw.text((x, self.top+25), line4, font=font, fill=255)
+        self.draw.text((x, self.top),    line1, font=self.font, fill=255)
+        self.draw.text((x, self.top+8),  line2, font=self.font, fill=255)
+        self.draw.text((x, self.top+16), line3, font=self.font, fill=255)
+        self.draw.text((x, self.top+25), line4, font=self.font, fill=255)
         # Display
         self.disp.image(self.image)
         self.disp.display()
--- program.py
+++ program.py
@@ -1,12 +1,17 @@
 import sys
 sys.path.insert(1, './Screen')
-import telegraph-display
+import TelegraphDisplay
+import sys, tty, termios
+
+
+
 #modes: 0-just type, 1-practice alpha, 2-practice digits, 3-practice alphanum, 4-practice symbols, 5-practice all
-mode=0
+mode=1
 userInput=""
 streak=0
 index=0
-screen = telegraph-display.Screen()
+text=""
+screen = TelegraphDisplay.Screen()
 
 lineLength=10
 
@@ -22,25 +27,29 @@ def loadText():
         text=f.read()
     return(text)
 
-def updateDisplay():
+def updateDisplay(): 
     #line 1
     if mode==0:
         line1=""
     else:
         if streak==0:
             #show hint
-            line1="hint: +"morseCode[text[index]]
+            line1="hint: "+morseCode[text[index].upper()]
         else:
             line1="streak: "+str(streak)
     #line 2
     fromIndex=max(0,index-lineLength)
     toIndex=min(len(text)-1,index+lineLength)
-    line2=text[fromIndex,toIndex]
+    line2=text[fromIndex:toIndex]
     #lines to display
+    print(line1+"\n"+line2)
     screen.writeLines(line1,line2)
 
 
 def updateVals():
+    global index
+    global text
+    global streak
     if mode==0:
         if userInput=="\b":
             #backspace
@@ -50,19 +59,41 @@ def updateVals():
             index+=1
             text+=userInput
     else:
-        if text[index]==userInput:
+        if text[index].upper()==userInput.upper():
             streak+=1
             index+=1
         else:
             streak=0
 
+# def getCharFromUser():
+#     #TODO
+#     global userInput
+#     userInput=input(" in ")[0].upper()
+
+def getCharFromUser():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    global userInput
+    userInput=str(ch).upper()
+
+
 def main():
+    global text
     print("Running...")
     text=loadText()
     while True:
         updateDisplay()
         getCharFromUser()
         updateVals()
+        print(userInput)
+        if userInput.upper() == '\x03':
+            #capture ctrl-c to exit
+            exit()
        
 
 if __name__ == "__main__":
